7b9407afe8d80182d70ac403d0e593e25c7bd31c
Natelou <mathieu.pecoul@laposte.net>
2019-12-17 12:54:39 +0100

    Gestion published et passage en dur

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -16,13 +16,22 @@ class PagesController extends AbstractController
     public function index(PagesRepository $pagesRepository)
     {
         //Je voudrais récupérer le titre de la page récupérer par le biais d'un bouton
-        //afin de le passer dans 'controller_name'
-        
+        //afin de le passer dans 'pages'
 
-        /*$pages = $this->getDoctrine()->getRepository(Pages::class)->findOneBy(array('titre' => ->getTitre));
-        */
+        
+        $user = $this->getUser();
+        $pages = new Pages;
+        if($user && !$pages){
+        $pages = $this->getDoctrine()->getRepository(Pages::class)->findOneBy(array('titre' => $user->getTitre()));
+        } else {
+            $pages->setTitre('CGU');
+            $pages->setContenu("Conditions Générales d'Utilisation");
+            $pages->setPublished(true);
+        }
+        
         return $this->render('pages/index.html.twig', [
             'controller_name' => 'PagesController',
+            'pages' => $pages,
         ]);
     }
 }
